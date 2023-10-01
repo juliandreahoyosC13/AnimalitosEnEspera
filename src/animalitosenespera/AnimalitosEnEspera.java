2c1f7fe1bdf744688ad49180fedcac0a62e8a12e
juliandreahoyosC13 <julianacodigo123@gmail.com>
2023-10-01 15:10:34 -0500

    Explorando subclases y su manejo en main con getter y setter

--- src/animalitosenespera/AnimalitosEnEspera.java
+++ src/animalitosenespera/AnimalitosEnEspera.java
@@ -7,8 +7,14 @@ public class AnimalitosEnEspera {
         Animal conejito = new Animal("Nube","Californiano","Mamifero",5,true);
         System.out.println("El conejito ingresado se llama: " + conejito.nombre);
         
-       
+        Gato gatito = new Gato();
+        gatito.setNombre("Orange");
+        gatito.setGrupo("Mamifero");
+        gatito.setRaza("Persa");
+        gatito.setEdad(9);
+        gatito.setVacunas(false);
         
+        System.out.println("El gatito ingresado se llama " + gatito.getNombre() + " y es de raza " + gatito.getRaza());
         
 
     }
